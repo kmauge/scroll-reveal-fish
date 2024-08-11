e6c63fdfb7e90144ec10cd74c482235ca73bf7a1
Mauge <35950762+kmauge@users.noreply.github.com>
2024-08-11 11:47:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.main-title, .section-title', { delay: 300, origin: 'left' });
+ScrollReveal().reveal('.sec-01 .img, .info', { delay: 400, origin: 'bottom' });
+ScrollReveal().reveal('.text-box', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.media-icons img', { delay: 300, origin: 'bottom', interval: 200 });
+ScrollReveal().reveal('.sec-02 .img, .sec-02 .img', { delay: 300, origin: 'top' });
+ScrollReveal().reveal('.media-info li', { delay: 300, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.media-icons img', { delay: 300, origin: 'bottom', interval: 200 });
